07ffc0dc3294ff9014576213d63b4869686ebfdf
Niels van Velzen <git@ndat.nl>
2022-09-06 17:31:26 +0200

    Use random device id when initializing QuickConnect
    (cherry picked from commit 02b9fb60bb15d33a2fc4759f0f5c08b4d22789ae)

--- app/src/main/java/org/jellyfin/androidtv/di/AppModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/di/AppModule.kt
@@ -89,7 +89,7 @@ val appModule = module {
 	single<NotificationsRepository> { NotificationsRepositoryImpl(get(), get()) }
 
 	viewModel { StartupViewModel(get(), get(), get(), get()) }
-	viewModel { UserLoginViewModel(get(), get(), get()) }
+	viewModel { UserLoginViewModel(get(), get(), get(), get(defaultDeviceInfo)) }
 	viewModel { ServerAddViewModel(get()) }
 	viewModel { NextUpViewModel(get(), get(), get(), get()) }
 	viewModel { PictureViewerViewModel(get()) }
