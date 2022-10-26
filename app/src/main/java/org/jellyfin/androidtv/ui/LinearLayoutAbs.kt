e56eb53e0dd2384346fb8a6de05d06d217be5110
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:32:21 +0200

    add LayoutAbs

---
+++ app/src/main/java/org/jellyfin/androidtv/ui/LinearLayoutAbs.kt
@@ -0,0 +1,54 @@
+package org.jellyfin.androidtv.ui
+
+import android.content.Context
+import android.graphics.drawable.ShapeDrawable
+import android.util.AttributeSet
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import me.carleslc.kotlin.extensions.number.roundDiv
+import org.jellyfin.androidtv.R
+
+open class LinearLayoutAbs @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+    private val heightPctPx: Int?
+    private val widthPctPx: Int?
+
+    init {
+        context.obtainStyledAttributes(attrs, R.styleable.LinearLayoutAbs, defStyleAttr, defStyleRes).run {
+            heightPctPx = getFraction(R.styleable.LinearLayoutAbs_heightPct, 1, 1, 0f).takeIf { it > 0 }?.let {
+                // NOTE: make even
+               (resources.displayMetrics.heightPixels * it).roundDiv(2).times(2).toInt()
+            }
+            widthPctPx = getFraction(R.styleable.LinearLayoutAbs_widthPct, 1, 1, 0f).takeIf { it > 0 }?.let {
+                // NOTE: make even
+                (resources.displayMetrics.widthPixels * it).roundDiv(2).times(2).toInt()
+            }
+
+            getDimensionPixelSize(R.styleable.LinearLayoutAbs_dividerSize, 0).takeIf { it > 0 }?.let {
+                dividerDrawable = ShapeDrawable().apply {
+                    if (orientation == HORIZONTAL)
+                        intrinsicWidth = it
+                    else
+                        intrinsicHeight = it
+                    alpha = 0
+                }
+                showDividers = SHOW_DIVIDER_MIDDLE
+            }
+
+            recycle()
+        }
+    }
+
+    override fun setLayoutParams(params: ViewGroup.LayoutParams?) {
+        params?.apply {
+            heightPctPx?.takeIf { height == 0 }?.let { height = heightPctPx }
+            widthPctPx?.takeIf { width == 0 }?.let { width = widthPctPx }
+        }
+        super.setLayoutParams(params)
+    }
+}
