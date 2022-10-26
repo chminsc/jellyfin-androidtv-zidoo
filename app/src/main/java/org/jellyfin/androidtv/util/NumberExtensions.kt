026912bfa70d10ef45bb9e4a5b7d5dd3143d6435
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:38:41 +0200

    add number helpers

--- app/src/main/java/org/jellyfin/androidtv/util/NumberExtensions.kt
+++ app/src/main/java/org/jellyfin/androidtv/util/NumberExtensions.kt
@@ -1,8 +1,37 @@
 package org.jellyfin.androidtv.util
 
 import android.content.Context
+import android.graphics.Color
+import androidx.annotation.ColorInt
+import androidx.annotation.FloatRange
+import org.jellyfin.androidtv.JellyfinApplication
+import kotlin.math.roundToInt
 
 /**
- * Current (pixel) value as display pixels
+ * Convert Dp to Pixel
  */
-fun Int.dp(context: Context): Int = Utils.convertDpToPixel(context, this)
+fun Int.dp(context: Context): Int = (context.resources.displayMetrics.density * this).roundToInt()
+fun Int.dp(): Int = (JellyfinApplication.appContext.resources.displayMetrics.density * this).roundToInt()
+
+/**
+ * Convert Sp to Pixel
+ */
+fun Int.sp(context: Context): Int = (context.resources.displayMetrics.scaledDensity * this).roundToInt()
+fun Int.sp(): Int = (JellyfinApplication.appContext.resources.displayMetrics.scaledDensity * this).roundToInt()
+
+fun Int.msToRuntimeTicks(): Long = (this * 10000).coerceAtLeast(0).toLong()
+fun Long.runtimeTicksToMs(): Int = (this / 10000).coerceAtLeast(0).toInt()
+
+@ColorInt
+fun @receiver:ColorInt Int.saturationHSV(@FloatRange(from = 0.0, to = 1.0) sValue: Float): Int = FloatArray(3).let {
+    Color.colorToHSV(this, it)
+    it[1] = sValue
+    Color.HSVToColor(it)
+}
+
+@ColorInt
+fun @receiver:ColorInt Int.valueHSV(@FloatRange(from = 0.0, to = 1.0) vValue: Float): Int = FloatArray(3).let {
+    Color.colorToHSV(this, it)
+    it[2] = vValue
+    Color.HSVToColor(it)
+}
