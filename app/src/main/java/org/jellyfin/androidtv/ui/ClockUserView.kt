fb800b3b1b14bc2235ac7aa4d3b12d409bf5a7da
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 07:38:18 +0200

    fix/optimize layout auto-adapt Clock textsize

--- app/src/main/java/org/jellyfin/androidtv/ui/ClockUserView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/ClockUserView.kt
@@ -2,8 +2,11 @@ package org.jellyfin.androidtv.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
-import android.widget.RelativeLayout
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import org.jellyfin.androidtv.R
@@ -14,6 +17,7 @@ import org.jellyfin.androidtv.preference.constant.ClockBehavior
 import org.jellyfin.androidtv.ui.playback.PlaybackOverlayActivity
 import org.jellyfin.androidtv.util.ImageUtils
 import org.jellyfin.androidtv.util.getActivity
+import org.jellyfin.androidtv.util.sp
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -22,28 +26,40 @@ class ClockUserView @JvmOverloads constructor(
 	attrs: AttributeSet? = null,
 	defStyleAttr: Int = 0,
 	defStyleRes: Int = 0,
-) : RelativeLayout(context, attrs, defStyleAttr, defStyleRes), KoinComponent {
+) : FrameLayout(context, attrs, defStyleAttr, defStyleRes), KoinComponent {
 	private val binding: ClockUserBugBinding = ClockUserBugBinding.inflate(LayoutInflater.from(context), this, true)
 	private val userPreferences by inject<UserPreferences>()
 	private val userRepository by inject<UserRepository>()
 
 	init {
-		val showClock = userPreferences[UserPreferences.clockBehavior]
+		layoutParams = LayoutParams(WRAP_CONTENT, MATCH_PARENT)
 
-		binding.clock.isVisible = when (showClock) {
-			ClockBehavior.ALWAYS -> true
-			ClockBehavior.NEVER -> false
-			ClockBehavior.IN_VIDEO -> context.getActivity() is PlaybackOverlayActivity
-			ClockBehavior.IN_MENUS -> context.getActivity() !is PlaybackOverlayActivity
+		if (!isInEditMode) {
+			val showClock = userPreferences[UserPreferences.clockBehavior]
+
+			binding.clock.isVisible = when (showClock) {
+				ClockBehavior.ALWAYS -> true
+				ClockBehavior.NEVER -> false
+				ClockBehavior.IN_VIDEO -> context.getActivity() is PlaybackOverlayActivity
+				ClockBehavior.IN_MENUS -> context.getActivity() !is PlaybackOverlayActivity
+			}
+
+			val currentUser = userRepository.currentUser.value
+
+			binding.clockUserImage.load(
+				url = currentUser?.let(ImageUtils::getPrimaryImageUrl),
+				fallback = ContextCompat.getDrawable(context, R.drawable.ic_user)
+			)
+
+			binding.clockUserImage.isVisible = currentUser != null
+		}
+	}
+
+	override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+		getDefaultSize(20.sp(context), heightMeasureSpec).let {
+			binding.clock.setTextSize(TypedValue.COMPLEX_UNIT_PX, (it - 8).coerceAtLeast(9.sp(context)).toFloat() )
 		}
 
-		val currentUser = userRepository.currentUser.value
-
-		binding.clockUserImage.load(
-			url = currentUser?.let(ImageUtils::getPrimaryImageUrl),
-			placeholder = ContextCompat.getDrawable(context, R.drawable.ic_user)
-		)
-
-		binding.clockUserImage.isVisible = currentUser != null
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec)
 	}
 }
