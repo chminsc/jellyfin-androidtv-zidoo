85f265bfc7b9028e2cc37673ddc8ce0fd1b44def
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:40:14 +0200

    adapt to new rowPresenter layout

--- app/src/main/java/org/jellyfin/androidtv/ui/presentation/PositionableListRowPresenter.java
+++ app/src/main/java/org/jellyfin/androidtv/ui/presentation/PositionableListRowPresenter.java
@@ -1,44 +1,28 @@
 package org.jellyfin.androidtv.ui.presentation;
 
-import androidx.leanback.widget.ListRowPresenter;
-import androidx.leanback.widget.RowPresenter;
-
 import timber.log.Timber;
 
 public class PositionableListRowPresenter extends CustomListRowPresenter {
-    private ListRowPresenter.ViewHolder viewHolder;
-
     public PositionableListRowPresenter() {
         super();
-        setShadowEnabled(false);
     }
 
-    public PositionableListRowPresenter(Integer padding) {
-        super(padding);
-        setShadowEnabled(false);
-    }
-
-    @Override
-    public boolean isUsingDefaultShadow() {
-        return false;
-    }
-
-    @Override
-    protected void onSelectLevelChanged(RowPresenter.ViewHolder holder) {
-        //Do nothing - this removes the shadow on the out of focus rows of image cards
-    }
-
-    @Override
-    protected void onBindRowViewHolder(RowPresenter.ViewHolder holder, Object item) {
-        super.onBindRowViewHolder(holder, item);
-
-        viewHolder = (ViewHolder) holder;
+    public PositionableListRowPresenter(int rowHeightPx, int headerHeightPx) {
+        super(rowHeightPx, headerHeightPx);
     }
 
     public void setPosition(int ndx) {
         Timber.d("Setting position to: %d", ndx);
+
         if (viewHolder != null && viewHolder.getGridView() != null)
             viewHolder.getGridView().setSelectedPosition(ndx);
+    }
+
+    public void setPositionSmooth(int ndx) {
+        Timber.d("Setting position to: %d", ndx);
+
+        if (viewHolder != null && viewHolder.getGridView() != null)
+            viewHolder.getGridView().setSelectedPositionSmooth(ndx);
     }
 
     public int getPosition() {
