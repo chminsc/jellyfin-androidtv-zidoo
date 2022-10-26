f30ea1f081fada23746f290142f4a15bdd4e5b27
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 07:45:45 +0200

    minor change

--- app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/DetailRowView.kt
@@ -2,8 +2,9 @@ package org.jellyfin.androidtv.ui
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.FrameLayout
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import org.jellyfin.androidtv.databinding.ViewRowDetailsBinding
 
 class DetailRowView @JvmOverloads constructor(
@@ -13,4 +14,8 @@ class DetailRowView @JvmOverloads constructor(
 	defStyleRes: Int = 0,
 ) : FrameLayout(context, attrs, defStyleAttr, defStyleRes) {
 	val binding = ViewRowDetailsBinding.inflate(LayoutInflater.from(context), this, true)
+
+	init {
+		layoutParams = LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+	}
 }
