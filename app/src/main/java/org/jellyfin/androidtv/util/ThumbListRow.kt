64bc17e720cfccf54500387801595a8c54e577a2
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:51:12 +0200

    add ListRow variant

---
+++ app/src/main/java/org/jellyfin/androidtv/util/ThumbListRow.kt
@@ -0,0 +1,9 @@
+package org.jellyfin.androidtv.util
+
+import androidx.leanback.widget.HeaderItem
+import androidx.leanback.widget.ListRow
+import androidx.leanback.widget.ObjectAdapter
+
+class ThumbListRow constructor(header: HeaderItem?, adapter: ObjectAdapter?)
+    : ListRow(header, adapter) {
+}
