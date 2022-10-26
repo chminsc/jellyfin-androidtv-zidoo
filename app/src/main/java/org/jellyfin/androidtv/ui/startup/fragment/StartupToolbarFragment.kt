f687e0b34579dd8659049e48153d04a153af88ea
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:44:48 +0200

    use new ToolbarView

--- app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/startup/fragment/StartupToolbarFragment.kt
@@ -10,32 +10,31 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import org.jellyfin.androidtv.R
-import org.jellyfin.androidtv.databinding.FragmentToolbarStartupBinding
+import org.jellyfin.androidtv.ui.ToolbarView
 import org.jellyfin.androidtv.ui.preference.PreferencesActivity
 import org.jellyfin.androidtv.ui.preference.screen.AuthPreferencesScreen
 
 class StartupToolbarFragment : Fragment() {
-	private lateinit var binding: FragmentToolbarStartupBinding
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-		binding = FragmentToolbarStartupBinding.inflate(inflater, container, false)
+		return ToolbarView(requireContext()).apply {
 
-		binding.help.setOnClickListener {
-			parentFragmentManager.commit {
-				addToBackStack(null)
-				replace<ConnectHelpAlertFragment>(R.id.content_view)
+			addButton(R.drawable.ic_help, R.string.help) {
+				parentFragmentManager.commit {
+					addToBackStack(null)
+					replace<ConnectHelpAlertFragment>(R.id.content_view)
+				}
 			}
-		}
 
-		binding.settings.setOnClickListener {
-			val intent = Intent(requireActivity(), PreferencesActivity::class.java)
-			intent.putExtra(PreferencesActivity.EXTRA_SCREEN, AuthPreferencesScreen::class.qualifiedName)
-			intent.putExtra(PreferencesActivity.EXTRA_SCREEN_ARGS, bundleOf(
-				AuthPreferencesScreen.ARG_SHOW_ABOUT to true
-			))
-			startActivity(intent)
-		}
+			addButton(R.drawable.ic_settings, R.string.lbl_settings) {
+				val intent = Intent(activity, PreferencesActivity::class.java)
+				intent.putExtra(PreferencesActivity.EXTRA_SCREEN, AuthPreferencesScreen::class.qualifiedName)
+				intent.putExtra(PreferencesActivity.EXTRA_SCREEN_ARGS, bundleOf(
+					AuthPreferencesScreen.ARG_SHOW_ABOUT to true
+				))
+				activity?.startActivity(intent)
+			}
 
-		return binding.root
+		}.rootView
 	}
 }
