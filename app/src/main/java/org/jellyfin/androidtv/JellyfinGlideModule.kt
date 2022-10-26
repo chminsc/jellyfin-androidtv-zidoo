a78b4896a7cfad359702407222a3c653663bccf6
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:26:06 +0200

    add clearDiskCache change Glide Cache strategy to "ALL" add auto Glide cache size logic

--- app/src/main/java/org/jellyfin/androidtv/JellyfinGlideModule.kt
+++ app/src/main/java/org/jellyfin/androidtv/JellyfinGlideModule.kt
@@ -2,19 +2,55 @@ package org.jellyfin.androidtv
 
 import android.content.Context
 import android.util.Log
+import com.bumptech.glide.Glide
 import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory
+import com.bumptech.glide.load.engine.cache.LruResourceCache
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.RequestOptions
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import me.carleslc.kotlin.extensions.bytes.bytes
+import me.carleslc.kotlin.extensions.bytes.gibibytes
+import me.carleslc.kotlin.extensions.bytes.megabytes
+import org.jellyfin.androidtv.util.Utils.getDeviceMemorySize
+import timber.log.Timber
+
 
 @GlideModule
 class JellyfinGlideModule : AppGlideModule() {
+	companion object {
+		suspend fun clearDiskCache() {
+			JellyfinApplication.appContext.let {
+				Timber.w("Clearing Glide Disk Cache !")
+				withContext(Dispatchers.IO) {
+					Glide.get(it).clearDiskCache()
+				}
+			}
+		}
+	}
+
 	override fun applyOptions(context: Context, builder: GlideBuilder): Unit = with(builder) {
+		// Set default disk cache strategy
 		setDefaultRequestOptions(
-			// Set default disk cache strategy
-			RequestOptions().diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+			RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
 		)
+//		val calculator = MemorySizeCalculator.Builder(context).setMemoryCacheScreens(10f).build()
+//		Timber.w("Glide Memory: <%s> MB", calculator.memoryCacheSize.bytes.toMegaBytes)
+
+		val memSize = getDeviceMemorySize(context)
+		val memCacheSize = when {
+			(memSize > 3.gibibytes.toBytes)  -> 256.megabytes.toBytes
+			(memSize < 1.gibibytes.toBytes)  -> 64.megabytes.toBytes
+			else -> 128.megabytes.toBytes
+		}
+		setMemoryCache(LruResourceCache(memCacheSize))
+		val diskCacheSize = 512.megabytes.toBytes
+		setDiskCache(InternalCacheDiskCacheFactory(context, diskCacheSize))
+
+		Timber.w("DeviceMemSize <${memSize.bytes.toMegaBytes} MB> -> MemCacheSize<${memCacheSize.bytes.toMegaBytes} MB> : DiskCacheSize<${diskCacheSize.bytes.toMegaBytes} MB>")
 
 		// Silence image load errors
 		setLogLevel(Log.ERROR)
