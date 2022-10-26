9b2cde1122020ee3bdc4e521a4a28d7a6e00c850
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 07:44:21 +0200

    add auto-size header logic

--- app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
+++ app/src/main/java/org/jellyfin/androidtv/ui/presentation/CustomRowHeaderPresenter.kt
@@ -1,9 +1,25 @@
 package org.jellyfin.androidtv.ui.presentation
 
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.leanback.widget.Presenter
 import androidx.leanback.widget.RowHeaderPresenter
+import org.jellyfin.androidtv.R
+import org.jellyfin.androidtv.util.setAutoTextSizeSingleLine
 
-class CustomRowHeaderPresenter : RowHeaderPresenter() {
-	override fun onSelectLevelChanged(holder: ViewHolder) {
-		// Do nothing - this keeps headers opaque
+class CustomRowHeaderPresenter constructor(
+	var headerHeightPx: Int,
+	var headerPaddingPx: Int
+) : RowHeaderPresenter() {
+
+	override fun onCreateViewHolder(parent: ViewGroup?): Presenter.ViewHolder {
+		return super.onCreateViewHolder(parent).apply {
+			view.layoutParams?.apply {
+				height = headerHeightPx
+			}
+			view.findViewById<TextView>(R.id.row_header)?.apply {
+				setAutoTextSizeSingleLine(headerHeightPx - headerPaddingPx)
+			}
+		}
 	}
 }
