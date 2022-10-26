bd7641ace6cf8d664529223b35f3133a6ce6fc7e
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:54:06 +0200

    add more helpers

--- app/src/main/java/org/jellyfin/androidtv/util/Utils.java
+++ app/src/main/java/org/jellyfin/androidtv/util/Utils.java
@@ -1,12 +1,19 @@
 package org.jellyfin.androidtv.util;
 
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
+
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.media.AudioManager;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import org.jellyfin.androidtv.preference.UserPreferences;
 import org.jellyfin.androidtv.preference.constant.AudioBehavior;
@@ -51,9 +58,25 @@ public class Utils {
         return convertDpToPixel(ctx, (float) dp);
     }
 
+    // NOTE: Zidoo default = 1.5 AndroidTV = 2.0
     public static int convertDpToPixel(@NonNull Context ctx, float dp) {
         float density = ctx.getResources().getDisplayMetrics().density;
         return Math.round(dp * density);
+    }
+
+    public static int convertPixelToDP(@NonNull Context ctx, float px) {
+        float density = ctx.getResources().getDisplayMetrics().density;
+        return Math.round(px / density);
+    }
+
+    public static int convertSpToPixel(@NonNull Context ctx, float sp) {
+        float density = ctx.getResources().getDisplayMetrics().scaledDensity;
+        return Math.round(sp * density);
+    }
+
+    public static int convertPixelToSP(@NonNull Context ctx, float px) {
+        float density = ctx.getResources().getDisplayMetrics().scaledDensity;
+        return Math.round(px / density);
     }
 
     public static boolean isTrue(Boolean value) {
@@ -194,5 +217,190 @@ public class Utils {
 
     public static boolean canManageRecordings(UserDto user) {
         return user != null && user.getPolicy().getEnableLiveTvManagement();
+    }
+
+    public static double lerp(double from, double to, double value, double minValue, double maxValue) {
+        double clampedRange = maxValue - minValue;
+        double clampedValue = Math.max(value - minValue, 0);
+        return (from + (to - from) * Math.min((clampedValue / clampedRange), 1.0));
+    }
+
+    public static int lerp(int from, int to, int value, int minValue, int maxValue) {
+        double clampedRange = maxValue - minValue;
+        double clampedValue = Math.max(value - minValue, 0);
+        return (int) Math.round(from + (to - from) * Math.min((clampedValue / clampedRange), 1.0));
+    }
+
+    public static void setViewPadding(@Nullable View view, Integer left, Integer top, Integer right, Integer bottom) {
+        if (view == null)
+            return;
+        view.setPadding(
+                left != null ? left : view.getPaddingLeft(),
+                top != null ? top : view.getPaddingTop(),
+                right != null ? right : view.getPaddingRight(),
+                bottom != null ? bottom : view.getPaddingBottom()
+        );
+    }
+
+    public static void setViewPaddingTop(@Nullable View view, int top) {
+        if (view == null)
+            return;
+        view.setPadding(
+                view.getPaddingLeft(),
+                top,
+                view.getPaddingRight(),
+                view.getPaddingBottom()
+        );
+    }
+
+    public static void setViewPaddingBottom(@Nullable View view, int bottom) {
+        if (view == null)
+            return;
+        view.setPadding(
+                view.getPaddingLeft(),
+                view.getPaddingTop(),
+                view.getPaddingRight(),
+                bottom
+        );
+    }
+
+    public static void setViewPaddingHorizontal(@Nullable View view, int hPadding) {
+        if (view == null)
+            return;
+        view.setPadding(
+                hPadding,
+                view.getPaddingTop(),
+                hPadding,
+                view.getPaddingBottom()
+        );
+    }
+
+    public static void setViewPaddingVertical(@Nullable View view, int vPadding) {
+        if (view == null)
+            return;
+        view.setPadding(
+                view.getPaddingLeft(),
+                vPadding,
+                view.getPaddingRight(),
+                vPadding
+        );
+    }
+
+    public static void setViewPaddingLeft(@Nullable View view, int left) {
+        if (view == null)
+            return;
+        view.setPadding(
+                left,
+                view.getPaddingTop(),
+                view.getPaddingRight(),
+                view.getPaddingBottom()
+        );
+    }
+
+    public static void setFrameLayoutMarginBottom(@Nullable View view, int marginBottom) {
+        if (view == null)
+            return;
+        var lp = (FrameLayout.LayoutParams) view.getLayoutParams();
+        if (lp != null) {
+            lp.bottomMargin = marginBottom;
+            view.setLayoutParams(lp);
+        }
+    }
+
+    public static void setConstrainedVerticalWeight(@Nullable View view, int weight) {
+        if (view == null)
+            return;
+        var lp = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+        if (lp != null) {
+            lp.verticalWeight = weight;
+            view.setLayoutParams(lp);
+        }
+    }
+
+    public static void addConstrainedVerticalWeight(@Nullable View view, int addedWeight) {
+        if (view == null)
+            return;
+        var lp = (ConstraintLayout.LayoutParams) view.getLayoutParams();
+        if (lp != null) {
+            lp.verticalWeight = lp.verticalWeight + addedWeight;
+            view.setLayoutParams(lp);
+        }
+    }
+
+    public static double parseDimensionRatioString(@NonNull String dimensionRatio) {
+        double dimensionRatioValue = Double.NaN;
+        int dimensionRatioSide = ConstraintLayout.LayoutParams.UNSET;
+        int len = dimensionRatio.length();
+        int commaIndex = dimensionRatio.indexOf(',');
+        if (commaIndex > 0 && commaIndex < len - 1) {
+            String dimension = dimensionRatio.substring(0, commaIndex);
+            if (dimension.equalsIgnoreCase("W")) {
+                dimensionRatioSide = ConstraintLayout.LayoutParams.HORIZONTAL;
+            } else if (dimension.equalsIgnoreCase("H")) {
+                dimensionRatioSide = ConstraintLayout.LayoutParams.VERTICAL;
+            }
+            commaIndex++;
+        } else {
+            commaIndex = 0;
+        }
+        int colonIndex = dimensionRatio.indexOf(':');
+        if (colonIndex >= 0 && colonIndex < len - 1) {
+            String nominator = dimensionRatio.substring(commaIndex, colonIndex);
+            String denominator = dimensionRatio.substring(colonIndex + 1);
+            if (nominator.length() > 0 && denominator.length() > 0) {
+                try {
+                    double nominatorValue = Double.parseDouble(nominator);
+                    double denominatorValue = Double.parseDouble(denominator);
+                    if (nominatorValue > 0 && denominatorValue > 0) {
+                        if (dimensionRatioSide == ConstraintLayout.LayoutParams.VERTICAL) {
+                            dimensionRatioValue = Math.abs(denominatorValue / nominatorValue);
+                        } else {
+                            dimensionRatioValue = Math.abs(nominatorValue / denominatorValue);
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    // Ignore
+                }
+            }
+        } else {
+            String r = dimensionRatio.substring(commaIndex);
+            if (r.length() > 0) {
+                try {
+                    dimensionRatioValue = Double.parseDouble(r);
+                } catch (NumberFormatException e) {
+                    // Ignore
+                }
+            }
+        }
+        return dimensionRatioValue;
+    }
+
+    public static int calcDiagonal(int width, int height) {
+        if (height == 0 && width == 0)
+            return 0;
+        return (int) sqrt(pow(height, 2) + pow(width, 2));
+    }
+
+    public static double clamp(double value, double minValue, double maxValue) {
+        return Math.max(Math.min(value, maxValue), minValue);
+    }
+
+    public static int clamp(int value, int minValue, int maxValue) {
+        return Math.max(Math.min(value, maxValue), minValue);
+    }
+
+    public static boolean equalsDelta(double first, double second, double delta) {
+        return Math.abs(first - second) < delta;
+    }
+
+    public static long getDeviceMemorySize(Context context) {
+        var actManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        var memInfo = new ActivityManager.MemoryInfo();
+        actManager.getMemoryInfo(memInfo);
+        return memInfo.totalMem;
+    }
+
+    public static void breakPoint() {
+        assert(Boolean.TRUE);
     }
 }
