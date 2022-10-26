42bcf9f8ac56c790edea29ed242b47f4be653a0f
Andy Walsh <andy.walsh44+github@gmail.com>
2022-10-26 08:30:23 +0200

    add ui-hash logic add new grid-size, card-layout options

--- app/src/main/java/org/jellyfin/androidtv/preference/LibraryPreferences.kt
+++ app/src/main/java/org/jellyfin/androidtv/preference/LibraryPreferences.kt
@@ -1,12 +1,16 @@
 package org.jellyfin.androidtv.preference
 
+import me.carleslc.kotlin.extensions.number.clamp
+import org.jellyfin.androidtv.constant.CardInfoType
+import org.jellyfin.androidtv.constant.CardSpacing
 import org.jellyfin.androidtv.constant.GridDirection
 import org.jellyfin.androidtv.constant.ImageType
-import org.jellyfin.androidtv.constant.PosterSize
 import org.jellyfin.androidtv.preference.constant.LanguagesAudio
+import org.jellyfin.androidtv.preference.constant.RatingType
 import org.jellyfin.androidtv.preference.store.DisplayPreferencesStore
 import org.jellyfin.preference.booleanPreference
 import org.jellyfin.preference.enumPreference
+import org.jellyfin.preference.intPreference
 import org.jellyfin.preference.stringPreference
 import org.jellyfin.sdk.api.client.ApiClient
 import org.jellyfin.sdk.model.api.SortOrder
@@ -20,10 +24,13 @@ class LibraryPreferences(
 	api = api,
 ) {
 	companion object {
-		val posterSize = enumPreference("PosterSize", PosterSize.MED)
+		val gridSize = intPreference("GridSize", 7)
 		val imageType = enumPreference("ImageType", ImageType.POSTER)
 		val gridDirection = enumPreference("GridDirection", GridDirection.VERTICAL)
 		val enableSmartScreen = booleanPreference("SmartScreen", false)
+		val ratingType = enumPreference("RatingType", RatingType.RATING_HIDDEN)
+		val cardInfoType = enumPreference("CardInfoType", CardInfoType.NO_INFO)
+		val cardSpacing = enumPreference("CardSpacing", CardSpacing.NORMAL)
 
 		// Filters
 		val filterFavoritesOnly = booleanPreference("FilterFavoritesOnly", false)
@@ -36,5 +43,29 @@ class LibraryPreferences(
 		// Audio settings
 		val enableAudioSettings = booleanPreference("EnableAudioSettings", false)
 		val audioLanguage = enumPreference("AudioLanguage", LanguagesAudio.AUTO)
+
+		fun getGridSizeChecked(size: Int, direction: GridDirection, imageType: ImageType): Int = when (direction) {
+			GridDirection.HORIZONTAL -> when (imageType) {
+				ImageType.POSTER -> size.clamp(2, 11)
+				ImageType.THUMB -> size.clamp(3, 15)
+				ImageType.BANNER -> size.clamp(4, 15)
+			}
+			GridDirection.VERTICAL -> {
+				when (imageType) {
+					ImageType.POSTER -> size.clamp(4, 21)
+					ImageType.THUMB -> size.clamp(3, 17)
+					ImageType.BANNER -> size.clamp(2, 9)
+				}
+			}
+		}
+	}
+
+	fun getUiSettingsHash(): Int {
+		return this[imageType].hashCode() +
+				this[gridDirection].hashCode() +
+				this[ratingType].hashCode() +
+				this[cardInfoType].hashCode() +
+				this[gridSize].hashCode() +
+				this[cardSpacing].hashCode()
 	}
 }
